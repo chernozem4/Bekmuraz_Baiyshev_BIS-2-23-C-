db0946e0f4c40f929d08c6d1417f98cde9ca6560
chernozem4 <gaftaf2034@gmail.com>
2024-10-18 19:30:12 +0200

    1 задание 2 урока

--- Bekmuraz/Program.cs
+++ Bekmuraz/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using System;
+public class Order
+{
+
+    public string Dish { get; set; }
+    public int Quantity { get; set; }
+    public int Price { get; set; }
+
+    public Order(string dish, int quantity, int price)
+    {
+        Dish = dish;
+        Quantity = quantity;
+        Price = price;
+    }
+
+    public int CalculateTotalCost()
+    {
+        return Quantity * Price;
+    }
+
+    public void DisplayOrderInfo()
+    {
+        Console.WriteLine($"Хавчик: {Dish}");
+        Console.WriteLine($"Количество: {Quantity}");
+        Console.WriteLine($"Цена: {Price:C}");
+        Console.WriteLine($"стоимость: {CalculateTotalCost():C}");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+
+        Order order = new Order("Негр в пене", 121221, 23);
+
+        order.DisplayOrderInfo();
+    }
+}
