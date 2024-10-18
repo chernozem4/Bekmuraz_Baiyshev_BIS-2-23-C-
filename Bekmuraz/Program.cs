997ab3635198419a8b12a25e8fcfaecdab78bca0
chernozem4 <gaftaf2034@gmail.com>
2024-10-18 19:50:37 +0200

    3 задание 2 урока

--- Bekmuraz/Program.cs
+++ Bekmuraz/Program.cs
@@ -1,38 +1,56 @@
 ﻿using System;
-public class Order
+
+class Book
 {
+    public string Title { get; set; }  // Название книги
+    public string Author { get; set; }  // Автор
+    public bool Thereis { get; set; }  // изначально хотел стрингом порешать, но потом понял, что тогда код будет сложнее и больше, т.к. придется писать дополнительные части "выдана" и "есть" и потом их соединиять, а я глупый, поэтому тупо подсмотрел в интернете что можно использовать булево значение, которое может хранить в себе true false и потом if else
 
-    public string Dish { get; set; }
-    public int Quantity { get; set; }
-    public int Price { get; set; }
-
-    public Order(string dish, int quantity, int price)
+    
+    // Конструктор
+    public Book(string title, string author, bool thereis)
     {
-        Dish = dish;
-        Quantity = quantity;
-        Price = price;
+        Title = title;
+        Author = author;
+        Thereis = thereis;
     }
-
-    public int CalculateTotalCost()
+    // Конкретно эта часть изменяет статус доступности книги, есть булево значение thereis и мы даем ему возможность изменится, создаем метод изменения, создаем новое болевое значение и присваем его thereis 
+    public void Change(bool Chupachups)
     {
-        return Quantity * Price;
+        
+        Thereis = Chupachups;
     }
-
-    public void DisplayOrderInfo()
+    // вывод нашей книги
+    public void DisplayInfo()
     {
-        Console.WriteLine($"Хавчик: {Dish}");
-        Console.WriteLine($"Количество: {Quantity}");
-        Console.WriteLine($"Цена: {Price:C}");
-        Console.WriteLine($"стоимость: {CalculateTotalCost():C}");
+        
+        Console.WriteLine($"Название: {Title}");
+        Console.WriteLine($"Автор: {Author}");
+        if (Thereis)
+        {
+            Console.WriteLine("Доступна");
+        }
+        else
+        {
+            Console.WriteLine("Выдана");
+        }
     }
 }
+
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
 
-        Order order = new Order("Негр в пене", 121221, 23);
+        Book book = new Book("Крутые яйца", "Пушкин эминем", true); //здесь мы можем изменить значение на фалс, и статус книги изменится
+        book.DisplayInfo();
+        //конекретно эта часть нам нужна для изменения статуса книги, если у нас там false, то здесь будет true 
+        //Console.WriteLine("\nстатус");
+        //book.Change(true);
+        //book.DisplayInfo();
 
-        order.DisplayOrderInfo();
+
+
     }
 }
+    
